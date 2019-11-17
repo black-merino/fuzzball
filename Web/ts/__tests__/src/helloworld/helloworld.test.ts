607af915e91a4c917cb6e3be8249a0b311f8eb31
Yuuki Mizukami <black_merino@msn.com>
2019-11-17 15:19:27 +0900

    jest executed !!!

--- Web/ts/__tests__/src/helloworld/helloworld.test.ts
+++ Web/ts/__tests__/src/helloworld/helloworld.test.ts
@@ -1,12 +1,21 @@
 import { HelloWorld } from "../../../src/helloworld/helloworld";
 
 describe("sample test", () => {
-  describe("helloworld", () => {
+  test("helloworld", () => {
     const str = "hello";
     const hw = new HelloWorld(str);
     const ret = hw.greet();
 
     expect(ret).toBe(str);
+  });
+  beforeAll(() => {
+    console.log("before all");
+  });
+  beforeEach(() => {
+    console.log("before each");
+  });
+  afterEach(() => {
+    console.log("after each");
   });
   afterAll(() => {
     console.log("after all");
