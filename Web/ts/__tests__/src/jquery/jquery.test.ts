3f12155fa7986e829f58366185c34a03b6adbae0
Yuuki Mizukami <black_merino@msn.com>
2019-11-25 01:59:32 +0900

    add tsconfig.json.

--- Web/ts/__tests__/src/jquery/jquery.test.ts
+++ Web/ts/__tests__/src/jquery/jquery.test.ts
@@ -1,39 +1,36 @@
 //
 // Try to use HTML document and jquery in Typescript
 //
-import * as $ from 'jquery';
+import $ = require("jquery");
 import { JQWrapper } from "../../../src/jquery/jqwrapper";
 
 describe("sample test", () => {
   test("jsdom", () => {
     const elem = document.createElement("div");
-    elem.innerHTML =
-      "<div id='c1'><div id='id1'><div/><div/><div id='c2'/>";
+    elem.innerHTML = "<div id='c1'><div id='id1'><div/><div/><div id='c2'/>";
 
     console.log(elem.outerHTML);
   });
 
   test("jquery", () => {
     const elem = document.createElement("div");
-    elem.innerHTML =
-      "<div id='c1'><div id='id1'><div/><div/><div id='c2'/>";
+    elem.innerHTML = "<div id='c1'><div id='id1'><div/><div/><div id='c2'/>";
 
     const $elem = $(elem);
 
-    $elem.attr('disabled','');
-    $elem.removeAttr('disabled');
+    $elem.attr("disabled", "");
+    $elem.removeAttr("disabled");
   });
 
   test("JQWrapper", () => {
-    (<any>(top)).$ = $;
+    // for constructor of JQWwapper.
+    (<any>top).$ = $;
 
     const elem = document.createElement("div");
-    elem.innerHTML =
-      "<div id='c1'><div id='id1'><div/><div/><div id='c2'/>";
+    elem.innerHTML = "<div id='c1'><div id='id1'><div/><div/><div id='c2'/>";
 
     const jqw = new JQWrapper(elem);
     jqw.enable();
     jqw.disable();
-
   });
 });
